e2190a8d22ef490d9032f21e7c577b922f0aaac1
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2024-04-07 22:29:35 +0200

    Error message what i cannot solve
    I receive this message but I already wrapped everything and it styill comes back:
    
    chunk-XBHG3ID7.js?v=56cefd00:19409 Uncaught Error: useRoutes() may be used only in the context of a <Router> component.
        at invariant (react-router-dom.js?v=56cefd00:209:11)
        at useRoutesImpl (react-router-dom.js?v=56cefd00:3411:34)
        at useRoutes (react-router-dom.js?v=56cefd00:3408:10)
        at Routes (react-router-dom.js?v=56cefd00:3998:10)
        at renderWithHooks (chunk-XBHG3ID7.js?v=56cefd00:12171:26)
        at mountIndeterminateComponent (chunk-XBHG3ID7.js?v=56cefd00:14921:21)
        at beginWork (chunk-XBHG3ID7.js?v=56cefd00:15902:22)
        at beginWork$1 (chunk-XBHG3ID7.js?v=56cefd00:19749:22)
        at performUnitOfWork (chunk-XBHG3ID7.js?v=56cefd00:19194:20)
        at workLoopSync (chunk-XBHG3ID7.js?v=56cefd00:19133:13)

---
+++ profile-app-client/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+import HomePage from './pages/HomePage';
+import IsAnon from './components/IsAnon';
+import IsPrivate from './components/IsPrivate';
+import Signup from './components/Singup';
+import Profile from './pages/ProfilePage';
+
+
+import './App.css'
+
+function App() {
+  return (
+    
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <Signup />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <Login />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <Profile />
+            </IsPrivate>
+          }
+        />
+      </Routes>
+    </div>
+    
+  );
+}
+  
+
+
+export default App;
--- profile-app-server/routes/user.routes.js
+++ profile-app-server/routes/user.routes.js
@@ -1,30 +1,45 @@
-const express = require("express");
-const router = express.Router();
-
-const {isAuthenticated} = require('../middlewares/jwt.middleware');
+const router = require('express').Router();
+const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
-router.get('/users', isAuthenticated, (req,res, next) => {
-
-    return res.json(req.payload);
+// GET /api/users -  Retrieves the current user
+router.get('/users', async (req, res, next) => {
+  try {
+   
+    const currentUser = await User.findById(req.payload._id);
+  
+    if (!currentUser) {
+ 
+      return res.status(404).json({ message: 'User not found.' });
+    }
+    return res.json(currentUser);
+  } catch (error) {
+    console.log('Error fetching current user:', error);
+    return res.status(500).json({ message: 'Error fetching current user' });
+  }
 });
 
-router.put('/users', isAuthenticated, (req, res, next) => {
-    const {image, username, campus, course} = req.body;
-
-    if(image === ''){
-        res.status(400).json({message:"Invalid image"});
-        return;
+// PUT /api/users 
+router.put('/users', async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.payload._id)) {
+    return res.status(400).json({ message: 'Specified user ID is not valid' });
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.payload._id,
+      { image: req.body.image },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
+    return res.json(updatedUser);
+  } catch (error) {
+    console.log('Error updating current user:', error);
+    return res.status(500).json({ message: 'Error updating current user' });
+  }
+});
 
-    const {user} = req.payload;
-    user.image = image;
-
-    User.findByIdAndUpdate(user._id, user)
-        .then(updatedUser => {
-
-        })
-})
 
 
 module.exports = router;
